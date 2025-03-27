2dc4a81fe4d20fca3b84da59e25e27cb4166cf67
Ray <lhy131120@gmail.com>
2025-03-27 17:33:43 +0800

    update index

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+
+document.addEventListener("DOMContentLoaded", () => {
+	const swipers = document.querySelectorAll(".albums .swiper");
+
+	swipers.forEach((swiper) => {
+		const baseConfig = {
+			allowTouchMove: false,
+			slidesPerView: "auto",
+			spaceBetween: 0,
+			speed: 35000,
+			loop: true,
+			autoplay: {
+				delay: 0,
+				disableOnInteraction: false,
+			},
+			centeredSlides: true,
+		};
+
+		const finalConfig = swiper.classList.contains("reverse")
+			? {
+					...baseConfig,
+					autoplay: {
+						...baseConfig.autoplay,
+						reverseDirection: true,
+					},
+        }
+			: baseConfig;
+
+		new Swiper(swiper, finalConfig);
+	});
+	// const swiper = new Swiper(albumsSwiper, {
+	// 	allowTouchMove: false,
+	// 	slidesPerView: "auto",
+	// 	speed: 15000,
+	// 	loop: true,
+	// 	autoplay: {
+	// 		delay: 0,
+	// 		disableOnInteraction: false,
+	// 	},
+	// 	centeredSlides: true,
+	// });
+});
